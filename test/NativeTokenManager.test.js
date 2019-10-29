8804f1e9536a29fbe30def257c791ff898651af6
hanyunx <hanyunx2@gmail.com>
2019-10-29 13:49:09 -0700

    Add test cases of token auction

--- test/NativeTokenManager.test.js
+++ test/NativeTokenManager.test.js
@@ -37,12 +37,26 @@ function revertEVM() {
 
 contract('NativeTokenManager', async (accounts) => {
   let manager;
+  let supervisor;
 
   beforeEach(async () => {
-    manager = await NativeTokenManager.new(`0x${'0'.repeat(40)}`, 0);
+    supervisor = accounts[0];
+    manager = await NativeTokenManager.new(supervisor, true);
   });
 
   it('should deploy correctly', async () => {
     assert.notEqual(manager.address, `0x${'0'.repeat(40)}`);
+  });
+
+  it('should handle new token bid sucessfully', async () => {
+    await manager.newTokenAuctionSetter(1000, 50, 10000, { from: accounts[0] });
+    console.log('sss', supervisor);
+
+    // Start a new token auction.
+    await manager.newTokenAuctionStart();
+
+    // One bidder place a bid.
+    manager.bidNewToken(990, 900, { from: accounts[1], value: 800 });
+    console.log('sssd', accounts[1]);
   });
 });
