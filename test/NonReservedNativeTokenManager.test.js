2be56b32f0db6557c077fa8e6fec017b36e98c55
Jie You <jieyou@quarkchain.org>
2019-10-31 13:20:44 -0700

    Add general native token manager for paying as gas (#1)
    * gasUtility
    
    * inline bid function
    
    * fix lint problem
    
    * minor modification
    
    * add test for gasUtility
    
    * add test for gasUtility
    
    * add more tests
    
    * add refund ratio and test payAsGasUtility function
    
    * Refactor

--- test/NonReservedNativeTokenManager.test.js
+++ test/NonReservedNativeTokenManager.test.js
@@ -2,11 +2,10 @@
 const assert = require('assert');
 const { promisify } = require('util');
 
-const NativeTokenManager = artifacts.require('./NativeTokenManager');
+const NonReservedNativeTokenManager = artifacts.require('./NonReservedNativeTokenManager');
 require('chai').use(require('chai-as-promised')).should();
 
 const revertError = 'VM Exception while processing transaction: revert';
-const toHex = web3.utils.asciiToHex;
 const toWei = i => web3.utils.toWei(String(i));
 const web3SendAsync = promisify(web3.currentProvider.send);
 
@@ -35,20 +34,20 @@ function revertEVM() {
   });
 }
 
-contract('NativeTokenManager', async (accounts) => {
+contract('NonReservedNativeTokenManager', async (accounts) => {
   let manager;
   let supervisor;
 
   beforeEach(async () => {
     supervisor = accounts[0];
-    manager = await NativeTokenManager.new(supervisor, true);
+    manager = await NonReservedNativeTokenManager.new(supervisor, true);
   });
 
   it('should deploy correctly', async () => {
     assert.notEqual(manager.address, `0x${'0'.repeat(40)}`);
   });
 
-  it('should handle new token bid sucessfully', async () => {
+  it('should handle new token bid successfully', async () => {
     await manager.newTokenAuctionSetter(5, 2, 7 * 3600 * 24, { from: accounts[0] });
 
     // Start a new token auction.
