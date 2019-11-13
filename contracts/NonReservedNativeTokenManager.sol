e27713cdca6ad890be1fdc3be1e8aa093fea6a02
hanyunx <hanyunx2@gmail.com>
2019-11-12 17:11:57 -0800

    remove useless return

--- contracts/NonReservedNativeTokenManager.sol
+++ contracts/NonReservedNativeTokenManager.sol
@@ -187,16 +187,15 @@ contract NonReservedNativeTokenManager {
         require(msg.sender == token.owner, "Only the owner can mint new token.");
 
         token.totalSupply += amount;
+        require(token.totalSupply >= amount, "Addition overflow.");
 
-        // TODO: precompiled contract to be called for minting
         uint256[3] memory input;
         input[0] = uint256(address(token.owner));
         input[1] = tokenId;
         input[2] = amount;
-        uint256 ret;
         /* solium-disable-next-line */
         assembly {
-            if iszero(call(not(0), 0x514b430004, 0, input, 0x60, ret, 0x20)) {
+            if iszero(call(not(0), 0x514b430004, 0, input, 0x60, 0, 0)) {
                 revert(0, 0)
             }
         }
