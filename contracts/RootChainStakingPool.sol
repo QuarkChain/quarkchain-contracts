806662bbb8eedece0acca6524dffaab052d2e258
FreshmanQ <xunand.quarkchain.org>
2020-10-06 00:32:30 -0700

    add RootChainStakingPool contract & test

---
+++ contracts/RootChainStakingPool.sol
@@ -0,0 +1,249 @@
+pragma solidity >0.4.99 <0.6.0;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract RootChainStakingPool {
+
+    using SafeMath for uint256;
+
+    struct StakerInfo {
+        uint256 stakes;
+        uint256 arrPos;
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of QKC
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * accQKCPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws stakes to the StakingPool. Here's what happens:
+        //   1. The pool's `accQKCPerShare` gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    uint256 constant MAX_BP = 10000;
+
+    mapping (address => StakerInfo) public stakerInfo;
+    address[] public stakers;
+    address public admin;
+    uint256 public minStakes;
+    string  public adminContactInfo;
+    uint256 public totalStakes;
+    uint256 public maxStakers;
+    uint256 public accQKCPershare;
+
+    // Miner fee rate in basis point.
+    address public miner;
+    string  public minerContactInfo;
+    uint256 public minerFeeRateBp;
+    uint256 public minerReward;
+    // Mining pool maintainer and corresponding fee structure.
+    address public poolMaintainer;
+    uint256 public poolMaintainerFeeRateBp;
+    uint256 public poolMaintainerFee;
+
+    event Update(uint256 x, uint256 y, uint256 z);
+
+    constructor(
+        address _miner,
+        string  memory _minerContactInfo,
+        address _admin,
+        string  memory _adminContactInfo,
+        address _poolMaintainer,
+        uint256 _minStakes,
+        uint256 _minerFeeRateBp,
+        uint256 _poolMaintainerFeeRateBp,
+        uint256 _maxStakers
+    )
+        public
+    {
+        require(_minerFeeRateBp <= MAX_BP, "Fee rate should be in basis point.");
+        require(_poolMaintainerFeeRateBp <= MAX_BP, "Fee rate should be in basis point.");
+        require(
+            _minerFeeRateBp + _poolMaintainerFeeRateBp <= MAX_BP,
+            "Fee rate should be in basis point."
+        );
+        require(
+            accQKCPershare == 0, "miner may only be initialized once"
+        );
+        miner = _miner;
+        minerContactInfo = _minerContactInfo;
+        admin = _admin;
+        adminContactInfo = _adminContactInfo;
+        poolMaintainer = _poolMaintainer;
+        minStakes = _minStakes;
+        minerFeeRateBp = _minerFeeRateBp;
+        poolMaintainerFeeRateBp = _poolMaintainerFeeRateBp;
+        maxStakers = _maxStakers;
+    }
+
+    modifier onlyMiner() {
+        require(msg.sender == miner, "Only miner can call this function.");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function.");
+        _;
+    }
+
+    modifier onlyPoolMaintainer() {
+        require(msg.sender == poolMaintainer, "Only pool maintainer can call this function.");
+        _;
+    }
+
+    function poolSize() public view returns (uint256) {
+        return stakers.length;
+    }
+
+    // Add stakes
+    function () external payable {
+        updateStakingPool();
+        StakerInfo storage info = stakerInfo[msg.sender];
+        // New staker
+        if (info.stakes == 0) {
+            require(msg.value >= minStakes, "Invalid stakes.");
+            require(stakers.length < maxStakers, "Too many stakers.");
+            info.arrPos = stakers.length;
+            stakers.push(msg.sender);
+        }
+        uint256 pending = info.stakes.mul(accQKCPershare).div(1e18).sub(info.rewardDebt);
+        info.stakes = info.stakes.add(pending).add(msg.value);
+        info.rewardDebt = info.stakes.mul(accQKCPershare).div(1e18);
+        totalStakes = totalStakes.add(msg.value);
+        emit Update(info.stakes, accQKCPershare, totalStakes);
+    }
+
+    function withdrawStakes(uint256 amount) public {
+        require(amount > 0, "Invalid withdrawal.");
+        updateStakingPool();
+        StakerInfo storage info = stakerInfo[msg.sender];
+        assert(stakers[info.arrPos] == msg.sender);
+        uint256 pending = info.stakes.mul(accQKCPershare).div(1e18).sub(info.rewardDebt);
+        emit Update(info.stakes, info.rewardDebt, totalStakes);
+        info.stakes = info.stakes.add(pending);
+        emit Update(info.stakes, pending, accQKCPershare);
+
+        require(info.stakes >= amount, "Should have enough stakes to withdraw.");
+        require(
+            info.stakes.sub(amount) == 0 || info.stakes.sub(amount) >= minStakes,
+            "Should satisfy minimum stakes."
+        );
+        info.rewardDebt = info.stakes.mul(accQKCPershare).div(1e18);
+        info.stakes = info.stakes.sub(amount);
+        totalStakes = totalStakes.sub(amount);
+
+        msg.sender.transfer(amount);
+
+        if (info.stakes == 0) {
+            stakerInfo[stakers[stakers.length.sub(1)]].arrPos = info.arrPos;
+            stakers[info.arrPos] = stakers[stakers.length.sub(1)];
+            stakers.length = stakers.length.sub(1);
+            delete stakerInfo[msg.sender];
+        }
+    }
+
+    function withdrawMinerReward() public onlyMiner {
+        updateStakingPool();
+        uint256 toWithdraw = minerReward;
+        minerReward = 0;
+        msg.sender.transfer(toWithdraw);
+    }
+
+    function transferMaintainerFee() public onlyPoolMaintainer {
+        updateStakingPool();
+        uint256 toTransfer = poolMaintainerFee;
+        poolMaintainerFee = 0;
+        msg.sender.transfer(toTransfer);
+    }
+
+    function updateMiner(address payable _miner) public onlyMiner {
+        updateStakingPool();
+        miner = _miner;
+    }
+
+    function updateMinerContactInfo(string memory _minerContactInfo) public onlyMiner {
+        minerContactInfo = _minerContactInfo;
+    }
+
+    function updateAdmin(address _admin) public onlyAdmin {
+        admin = _admin;
+    }
+
+    function updateAdminContactInfo(string memory _adminContactInfo) public onlyAdmin {
+        adminContactInfo = _adminContactInfo;
+    }
+
+    function updatePoolMaintainer(address payable _poolMaintainer) public onlyPoolMaintainer {
+        updateStakingPool();
+        poolMaintainer = _poolMaintainer;
+    }
+
+    function adjustMinerFeeRate(uint256 _minerFeeRateBp) public onlyAdmin {
+        require(_minerFeeRateBp <= MAX_BP, "Fee rate should be in basis point.");
+        require(
+            _minerFeeRateBp + poolMaintainerFeeRateBp <= MAX_BP,
+            "Fee rate should be in basis point."
+        );
+        updateStakingPool();
+        minerFeeRateBp = _minerFeeRateBp;
+    }
+
+    function calculateStakesWithDividend(address staker) public view returns (uint256) {
+        if (totalStakes == 0) {
+            return 0;
+        }
+        uint256 dividend = getDividend(address(this).balance);
+        uint256 feeRateBp = minerFeeRateBp + poolMaintainerFeeRateBp;
+        uint256 stakerPayout = dividend.mul(MAX_BP - feeRateBp).div(MAX_BP);
+        uint256 tempAccQKCPershare = accQKCPershare.add(stakerPayout.mul(1e18).div(totalStakes));
+        StakerInfo storage info = stakerInfo[staker];
+        uint256 pending = info.stakes.mul(tempAccQKCPershare).div(1e18).sub(info.rewardDebt);
+        return info.stakes.add(pending);
+    }
+
+    function estimateMinerReward() public view returns (uint256) {
+        uint256 dividend = getDividend(address(this).balance).mul(minerFeeRateBp).div(
+            stakers.length > 0 ? MAX_BP : minerFeeRateBp + poolMaintainerFeeRateBp);
+        return minerReward.add(dividend);
+    }
+
+    function estimatePoolMaintainerFee() public view returns (uint256) {
+        uint256 dividend = getDividend(address(this).balance).mul(poolMaintainerFeeRateBp).div(
+            stakers.length > 0 ? MAX_BP : minerFeeRateBp + poolMaintainerFeeRateBp);
+        return poolMaintainerFee.add(dividend);
+    }
+
+    function updateStakingPool() private {
+        // When adding stakes, need to exclude the current message
+        uint256 balance = address(this).balance.sub(msg.value);
+        uint256 dividend = getDividend(balance);
+        if (dividend == 0) {
+            return;
+        }
+        uint256 feeRateBp = minerFeeRateBp + poolMaintainerFeeRateBp;
+        uint256 stakerPayout = dividend.mul(MAX_BP - feeRateBp).div(MAX_BP);
+        uint256 totalPaid = 0;
+        // Update accPershare
+        if (totalStakes != 0) {
+            accQKCPershare = accQKCPershare.add(stakerPayout.mul(1e18).div(totalStakes));
+            totalPaid = stakerPayout;
+        }
+        totalStakes = totalStakes.add(totalPaid);
+        uint256 totalFee = dividend.sub(totalPaid);
+        uint256 feeForMiner = totalFee.mul(minerFeeRateBp).div(feeRateBp);
+        uint256 feeForMaintainer = totalFee.sub(feeForMiner);
+        poolMaintainerFee = poolMaintainerFee.add(feeForMaintainer);
+        minerReward = minerReward.add(feeForMiner);
+        assert(balance >= totalStakes);
+    }
+
+    function getDividend(uint256 balance) private view returns (uint256) {
+        uint256 recordedAmount = totalStakes.add(minerReward).add(poolMaintainerFee);
+        require(balance >= recordedAmount, "Should have enough balance.");
+        return balance.sub(recordedAmount);
+    }
+}
---
+++ test/RootChainStakingPool.test.js
@@ -0,0 +1,313 @@
+/* eslint no-unused-vars: 0 */
+const assert = require('assert');
+const { promisify } = require('util');
+
+const RootChainStakingPool = artifacts.require('./RootChainStakingPool');
+const SelfDestruct = artifacts.require('./mocks/SelfDestruct');
+require('chai').use(require('chai-as-promised')).should();
+
+const revertError = 'VM Exception while processing transaction: revert';
+const toWei = i => web3.utils.toWei(String(i));
+const gasPriceMax = 0;
+
+function txGen(from, value) {
+  return {
+    from, value, gasPrice: gasPriceMax,
+  };
+}
+
+async function forceSend(target, value, from) {
+  const selfDestruct = await SelfDestruct.new({ value, from });
+  await selfDestruct.forceSend(target);
+}
+
+contract('RootChainStakingPool', async (accounts) => {
+  let pool;
+  const miner = accounts[9];
+  const minerContactInfo = 'miner@stakingpool';
+  const admin = accounts[8];
+  const adminContactInfo = 'admin@stakingpool';
+  const maintainer = accounts[7];
+  const treasury = accounts[6];
+  // 50% miner fee rate.
+  const minerFeeRateBp = 5000;
+  // None goes to maintainer.
+  const poolMaintainerFeeRateBp = 0;
+  const maxStakers = 16;
+  const minStakes = toWei(1);
+
+  beforeEach(async () => {
+    pool = await RootChainStakingPool.new(
+      miner,
+      minerContactInfo,
+      admin,
+      adminContactInfo,
+      maintainer,
+      minStakes,
+      minerFeeRateBp,
+      poolMaintainerFeeRateBp,
+      maxStakers,
+    );
+  });
+
+  it('should deploy correctly', async () => {
+    assert.notEqual(pool.address, `0x${'0'.repeat(40)}`);
+  });
+
+  it('should work with minStakes correctly', async () => {
+    const min = await pool.minStakes();
+    assert.equal(min, toWei(1));
+
+    await pool.sendTransaction(txGen(accounts[0], toWei(0.1)))
+      .should.be.rejectedWith(revertError);
+
+    await pool.sendTransaction(txGen(accounts[0], toWei(1)));
+    const stakerInfo = await pool.stakerInfo(accounts[0]);
+    assert.equal(stakerInfo[0], toWei(1));
+    assert.equal(stakerInfo[2], 0);
+
+    await pool.withdrawStakes(toWei(0.1)).should.be.rejectedWith(revertError);
+    await pool.withdrawStakes(toWei(1));
+  });
+
+  it('should handle adding stakes properly', async () => {
+    await pool.sendTransaction(txGen(accounts[0], toWei(42)));
+    const minerReward = await pool.minerReward();
+    assert.equal(minerReward, 0);
+    let poolSize = await pool.poolSize();
+    assert.equal(poolSize, 1);
+    const staker = await pool.stakers(0);
+    assert.equal(staker, accounts[0]);
+    const stakerInfo = await pool.stakerInfo(accounts[0]);
+    assert.equal(stakerInfo[0], toWei(42));
+    assert.equal(stakerInfo[1], 0);
+    assert.equal(stakerInfo[2], 0);
+    let totalStakes = await pool.totalStakes();
+    assert.equal(totalStakes, toWei(42));
+    let stakesWithDividends = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakesWithDividends, toWei(42));
+    // Random person should have zero stakes.
+    stakesWithDividends = await pool.calculateStakesWithDividend(accounts[5]);
+    assert.equal(stakesWithDividends, 0);
+
+    await pool.sendTransaction(txGen(accounts[1], toWei(100)));
+    poolSize = await pool.poolSize();
+    assert.equal(poolSize, 2);
+    totalStakes = await pool.totalStakes();
+    assert.equal(totalStakes, toWei(142));
+  });
+
+  it('should handle withdrawing stakes properly', async () => {
+    await pool.sendTransaction(txGen(accounts[0], toWei(42)));
+    await pool.withdrawStakes(toWei(0)).should.be.rejectedWith(revertError);
+    await pool.withdrawStakes(toWei(40));
+    let totalStakes = await pool.totalStakes();
+    assert.equal(totalStakes, toWei(2));
+    let poolBalance = await web3.eth.getBalance(pool.address);
+    assert.equal(poolBalance, toWei(2));
+    const stakerInfo = await pool.stakerInfo(accounts[0]);
+    let stakesWithDividends = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakesWithDividends, toWei(2));
+    // Failure.
+    await pool.withdrawStakes(toWei(3))
+      .should.be.rejectedWith(revertError);
+    // Withdraw all.
+    await pool.withdrawStakes(toWei(2));
+    const poolSize = await pool.poolSize();
+    assert.equal(poolSize, 0);
+    totalStakes = await pool.totalStakes();
+    assert.equal(totalStakes, 0);
+    stakesWithDividends = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakesWithDividends, 0);
+    poolBalance = await web3.eth.getBalance(pool.address);
+    assert.equal(poolBalance, 0);
+  });
+
+  it('should calculate dividends correctly', async () => {
+    await pool.sendTransaction(txGen(accounts[0], toWei(42)));
+    await forceSend(pool.address, toWei(8), treasury);
+    let poolBalance = await web3.eth.getBalance(pool.address);
+    assert.equal(poolBalance, toWei(50));
+    // State has not been updated.
+    let minerReward = await pool.minerReward();
+    assert.equal(minerReward, 0);
+    const stakerInfo = await pool.stakerInfo(accounts[0]);
+    let stakes = stakerInfo[0];
+    assert.equal(stakes, toWei(42));
+    assert.equal(stakerInfo[2], 0);
+    // Stakers can calculate their stakes with dividends.
+    stakes = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakes, '45999999999999999996');
+    // Calculate amount of dividends and let the staker withdraw, which will update the state.
+    const stakesWithDividends = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakesWithDividends, '45999999999999999996');
+    await pool.withdrawStakes(toWei(46.1)).should.be.rejectedWith(revertError);
+    await pool.withdrawStakes('45999999999999999996');
+    // 50% of coinbase rewards goes to miner.
+    minerReward = await pool.minerReward();
+    assert.equal(minerReward, toWei(4));
+    // Pool balance should update.
+    poolBalance = await web3.eth.getBalance(pool.address);
+    assert.equal(poolBalance, '4000000000000000004');
+    // Miner can withdraw as well.
+    await pool.withdrawMinerReward({ from: miner });
+    poolBalance = await web3.eth.getBalance(pool.address);
+    assert.equal(poolBalance, 4);
+    minerReward = await pool.minerReward();
+    assert.equal(minerReward, 0);
+  });
+
+  it('should accept new stakers', async () => {
+    await pool.sendTransaction(txGen(accounts[0], toWei(10)));
+    await forceSend(pool.address, toWei(10), treasury);
+    // Now the staker should have 15 QKC and the miner has 5.
+    // A new staker comes in.
+    const newStaker = accounts[2];
+    await pool.sendTransaction(txGen(newStaker, toWei(20)));
+    // Shouldn't affect the outcome: prev staker 15 QKC, miner 5 and new staker 20.
+    // 1. Check prev staker.
+    // Internal state not updated but should be able to withdraw stakes + dividends.
+    let stakerInfo = await pool.stakerInfo(accounts[0]);
+    let stakes = stakerInfo[0];
+    assert.equal(stakes, toWei(10));
+    assert.equal(stakerInfo[2], 0);
+    stakes = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakes, toWei(15));
+    // 2. Check new staker.
+    stakerInfo = await pool.stakerInfo(newStaker);
+    stakes = stakerInfo[0];
+    assert.equal(stakes, toWei(20));
+    assert.equal(stakerInfo[2], toWei(10));
+    stakes = await pool.calculateStakesWithDividend(newStaker);
+    assert.equal(stakes, toWei(20));
+    // 3. Check miner.
+    const minerBalanceBefore = await web3.eth.getBalance(miner);
+    let minerReward = await pool.minerReward();
+    assert.equal(minerReward, toWei(5));
+    // But should be able to withdraw his/her rewards.
+    await pool.withdrawMinerReward({ from: miner, gasPrice: 0 });
+    const minerBalanceAfter = await web3.eth.getBalance(miner);
+    const diff = (minerBalanceAfter / (10 ** 18)) - (minerBalanceBefore / (10 ** 18));
+    assert.equal(diff, 5);
+    // Prev staker should also be able to withdraw stakes + dividends.
+    await pool.withdrawStakes(toWei(15));
+    // After a new round of mining rewards, only the miner and the new staker should have dividends.
+    await forceSend(pool.address, toWei(20), treasury);
+    stakes = await pool.calculateStakesWithDividend(newStaker);
+    assert.equal(stakes, toWei(20 + (20 / 2)));
+    stakes = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakes, 0);
+    minerReward = await pool.minerReward();
+    assert.equal(minerReward, toWei(0));
+    minerReward = await pool.estimateMinerReward();
+    assert.equal(minerReward, toWei(20 / 2));
+  });
+
+  it('should handle no staker case', async () => {
+    await forceSend(pool.address, toWei(10), treasury);
+    let minerReward = await pool.minerReward();
+    assert.equal(minerReward, toWei(0));
+    minerReward = await pool.estimateMinerReward();
+    assert.equal(minerReward, toWei(10));
+    // Now adding a new staker, while miner's fee shouldn't be affected.
+    await pool.sendTransaction(txGen(accounts[0], toWei(10)));
+    minerReward = await pool.minerReward();
+    assert.equal(minerReward, toWei(10));
+    minerReward = await pool.estimateMinerReward();
+    assert.equal(minerReward, toWei(10));
+    // Now, new rewards should be distributed evenly.
+    await forceSend(pool.address, toWei(4), treasury);
+    minerReward = await pool.estimateMinerReward();
+    assert.equal(minerReward, toWei(10 + (4 / 2)));
+    const stakes = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakes, toWei(10 + (4 / 2)));
+  });
+
+  it('should allow admin to update fee rate', async () => {
+    let minerFeeRate = await pool.minerFeeRateBp();
+    assert.equal(minerFeeRate, 5000);
+    // Fail if not admin.
+    await pool.adjustMinerFeeRate(1000)
+      .should.be.rejectedWith(revertError);
+    // Fail if rate out of range.
+    await pool.adjustMinerFeeRate(10001, { from: admin })
+      .should.be.rejectedWith(revertError);
+    // Succeed.
+    await pool.adjustMinerFeeRate(1000, { from: admin });
+    minerFeeRate = await pool.minerFeeRateBp();
+    assert.equal(minerFeeRate, 1000);
+  });
+
+  it('should handle maintainer fee correctly', async () => {
+    // Start a new pool where the pool takes 12.5% while the miner takes 50%.
+    // eslint-disable-next-line max-len
+    pool = await RootChainStakingPool.new(miner, minerContactInfo, admin, adminContactInfo, maintainer, minStakes, minerFeeRateBp, 1250, maxStakers);
+    await pool.sendTransaction(txGen(accounts[0], toWei(1)));
+    await forceSend(pool.address, toWei(8), treasury);
+    // Stakes should be calculated correctly.
+    const stakes = await pool.calculateStakesWithDividend(accounts[0]);
+    assert.equal(stakes, toWei(1 + 3));
+    // State has not been updated. Estimate should work.
+    assert.equal((await pool.poolMaintainerFee()), 0);
+    assert.equal((await pool.estimatePoolMaintainerFee()), toWei(1));
+    assert.equal((await pool.minerReward()), 0);
+    assert.equal((await pool.estimateMinerReward()), toWei(4));
+    // Trigger a state update. Pool should have 4 while miner has 0.
+    await pool.withdrawMinerReward({ from: miner, gasPrice: 0 });
+    assert.equal((await pool.poolMaintainerFee()), toWei(1));
+    assert.equal((await pool.minerReward()), 0);
+    // Pool withdraws transfers the maintaining fee.
+    await pool.transferMaintainerFee({ from: maintainer, gasPrice: 0 });
+    assert.equal((await pool.poolMaintainerFee()), 0);
+    const maintainerBalance = await web3.eth.getBalance(maintainer);
+    assert.equal(maintainerBalance, toWei(100 + (8 / 8)));
+  });
+
+  it('should handle no staker case with pool maintainer', async () => {
+    // Start a new pool where the pool takes 12.5% while the miner takes 50%.
+    // eslint-disable-next-line max-len
+    pool = await RootChainStakingPool.new(miner, minerContactInfo, admin, adminContactInfo, maintainer, minStakes, minerFeeRateBp, 1250, maxStakers);
+
+    await forceSend(pool.address, toWei(10), treasury);
+    // Miner should take 50/(50+12.5) * 10 = 8.
+    assert.equal((await pool.minerReward()), toWei(0));
+    assert.equal((await pool.estimateMinerReward()), toWei(8));
+    // While maintainer should take 12.5/(50+12.5) * 10 = 2;
+    assert.equal((await pool.poolMaintainerFee()), 0);
+    assert.equal((await pool.estimatePoolMaintainerFee()), toWei(2));
+  });
+
+  it('should update miner and admin contact infomation correctly', async () => {
+    assert.equal(((await pool.minerContactInfo())), minerContactInfo);
+    assert.equal(((await pool.adminContactInfo())), adminContactInfo);
+
+    // Fail if not miner
+    await pool.updateMinerContactInfo('miner0@stakingpool')
+      .should.be.rejectedWith(revertError);
+    // Fail if not admin
+    await pool.updateAdminContactInfo('admin0@stakingpool')
+      .should.be.rejectedWith(revertError);
+    // Succeed
+    await pool.updateMinerContactInfo('miner0@stakingpool', { from: miner });
+    assert.equal(((await pool.minerContactInfo())), 'miner0@stakingpool');
+    await pool.updateAdminContactInfo('admin0@stakingpool', { from: admin });
+    assert.equal(((await pool.adminContactInfo())), 'admin0@stakingpool');
+  });
+
+  it('should update admin and pool maintainer correctly', async () => {
+    assert.equal(((await pool.admin())), admin);
+    assert.equal(((await pool.poolMaintainer())), maintainer);
+
+    // Fail if not admin
+    await pool.updateAdmin(accounts[4])
+      .should.be.rejectedWith(revertError);
+    // Fail if not poolMaintainer
+    await pool.updatePoolMaintainer(accounts[3])
+      .should.be.rejectedWith(revertError);
+    // Succeed
+    await pool.updateAdmin(accounts[4], { from: admin });
+    assert.equal(((await pool.admin())), accounts[4]);
+    await pool.updatePoolMaintainer(accounts[3], { from: maintainer });
+    assert.equal(((await pool.poolMaintainer())), accounts[3]);
+  });
+});
